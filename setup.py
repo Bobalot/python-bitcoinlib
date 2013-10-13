04a34bce2467010b98a3f48d6e55d94f4e9193d0
Robert Williamson <bobalot@gmail.com>
2013-10-13 17:29:37 +0100

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='python-bitcoinlib',
+      version='0.1',
+      description='python-bitcoinlib',
+      url='https://github.com/jgarzik/python-bitcoinlib/',
+      packages=['bitcoin'],
+     )
